3c2687d2cfeef2a0d6a15adc27549c7e0365ff7f
Kio <46105187+kio-grnd@users.noreply.github.com>
2024-11-10 04:59:31 -0300

    Update

--- scripts/install-bspwm.sh
+++ scripts/install-bspwm.sh
@@ -11,7 +11,7 @@ pkg update -y || { echo "Error al actualizar los repositorios"; exit 1; }
 pkg install -y nano git bash sudo htop vim ninja cmake curl wget bash-completion zsh zsh-completions \
 zsh-syntax-highlighting zsh-autosuggestions fusefs-ntfs fusefs-ext2 xorg xrandr xkill xinit xsetroot \
 nvidia-driver-470 nvidia-settings nvidia-xconfig font-awesome bspwm sxhkd polybar \
-rofi lxappearance feh picom rxvt-unicode zathura unzip zathura-pdf-poppler || { echo "Error al instalar los paquetes"; exit 1; }
+rofi lxappearance feh picom rxvt-unicode py311-ueberzug py311-ranger zathura unzip zathura-pdf-poppler || { echo "Error al instalar los paquetes"; exit 1; }
 
 # Configurar el driver de NVIDIA
 nvidia-xconfig
